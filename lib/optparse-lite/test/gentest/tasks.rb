12a610e1d5d2eb5be87702b8462c8df4749cb752
Mark Meves <mark.meves@gmail.com>
2010-05-13 06:35:20 -0400

    * move: colorization crap nearer to tasks

--- lib/optparse-lite/test/gentest/tasks.rb
+++ lib/optparse-lite/test/gentest/tasks.rb
@@ -2,6 +2,13 @@ if ! Object.const_defined?(:Hipe) || ! ::Hipe.const_defined?(:GenTest)
   require File.expand_path('../gentest.rb', __FILE__)
 end
 
+module Hipe::GenTest
+  def task_prefix
+    "\e[35mgentest\e[0m "
+  end
+end
+
 here = File.dirname(__FILE__)
+
 require here + '/tasks/gentest.rb'
 require here + '/tasks/ungen.rb'
