ab5d82853ae15886e59dc997573acd9882aa168f
Mark Meves <mark.meves@gmail.com>
2010-05-28 18:25:45 -0400

    * change: page-specific js for draggable

--- mysite/content/js/high-concept.js
+++ mysite/content/js/high-concept.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
   jQuery('.square.ruby').vizzle_when_you_mouseover_it_changes_color('#ff00dd');
   jQuery('.square.app').vizzle_when_you_mouseover_it_changes_color('#66bbff');
+	jQuery('.square').draggable();
 });
