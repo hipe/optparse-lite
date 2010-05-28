dc25d1dcb6c65c2231a52eaaf3c806ce26fc8cb6
Mark Meves <mark.meves@gmail.com>
2010-05-27 21:00:29 -0400

    * add: hacking sinatra action for outputting the diff

--- whatever/differ-service.rb
+++ whatever/differ-service.rb
@@ -1,5 +1,30 @@
 require 'sinatra'
 
+
+get '/diff-1.html' do
+  require File.dirname(__FILE__)+'/differ-service/differ'
+  some_root = File.dirname(__FILE__)+'/public/js'
+  left = some_root + '/jquery-1.4.3.pre.js'
+  right = some_root + '/jquery-1.4.3.pre-hact.js'
+  diff = Hipe::Differ.get_diff(left, right)
+  html = "<html><head><title>jquery diff</title>\n"
+  html <<
+  '<link rel="stylesheet" type="text/css" href="/css/trollop-subset.css" media="screen" />'
+  html << "\n</head>\n"
+  html << <<-HTML
+    <p>This is a diff showing the hacks I did to jquery to get it to work with xhtml and svgs.</p>
+    <em><a href='/'>back to main page</a></em>
+    <br />
+    <br />
+  HTML
+  html << "<body><pre id='the-diff' class='terminal'>\n"
+  html << diff.to_html
+  html << "</pre>\n"
+  html << "<div class='footer'><em>generated on #{Time.now}</em></div>\n"
+  html << "</body></html>\n"
+  html
+end
+
 # use index.html or index.xml if it exists, else passthru
 # thanks namelessjon
 #
@@ -17,6 +42,3 @@ get '/*' do
   end
 end
 
-get '/diff/:html' do |x|
-  "ok there is hope: #{Time.now} - #{x.inspect}"
-end
