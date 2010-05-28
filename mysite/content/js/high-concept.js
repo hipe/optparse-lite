65d4ba2b40ae8a7de63332ca3177e4ba59d4c0a3
Mark Meves <mark.meves@gmail.com>
2010-05-28 16:43:38 -0400

    smell: add page-specific js for high-concept.md -- hope that that diffy thing works for this

---
+++ mysite/content/js/high-concept.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function(){
+  jQuery('.square.ruby').vizzle_when_you_mouseover_it_changes_color('#ff00dd');
+  jQuery('.square.app').vizzle_when_you_mouseover_it_changes_color('#66bbff');
+});
