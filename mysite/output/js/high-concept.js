ece9cd186421112d17c4aac753af0ae8cf14a362
Mark Meves <mark.meves@gmail.com>
2010-06-13 08:07:23 -0400

    * add: js for silly spin thing

--- mysite/output/js/high-concept.js
+++ mysite/output/js/high-concept.js
@@ -81,4 +81,61 @@ jQuery(document).ready(function(){
     arrowBorder.attr('fill', '#aaaaaa');
     arrow.attr('fill','#ffffff');
 	});
+
+
+	// ### silly cute spin thing
+	var transformMe = $('#thing-to-spin');
+	var rotateThis, rotateThisNow;
+	var rotating = false;
+	var stopIt;
+	var rotateLeft = function(){
+		if (rotating) {
+			stopIt();
+		} else {
+			rotateThis(transformMe, -360, 142, 50, 1500, 18);
+		}
+	};
+	var rotateRight = function(){
+		if (rotating) {
+			stopIt();
+		} else {
+			rotateThis(transformMe, 360, 142, 50, 1500, 18);
+		}
+	};
+	var stopIt = function(){
+		rotateThisNow(transformMe, 0);
+		rotating = false;
+	};
+	rotateThis = function(elem, angle, cx, cy, timespan, fps){
+		if (rotating) { return; } // just sanity
+		rotating = true;
+		var msPerClick = 1000.0 / fps;
+		var numClicks = timespan / msPerClick;
+		var anglePerClick = (1.0 * angle) / numClicks;
+		var thisAndTheNext;
+		var currentAngle = 0.0;
+		done = ( angle < 0 ) ?
+		  function(){ return (currentAngle + anglePerClick) < angle; } :
+		  function(){ return (currentAngle + anglePerClick) > angle; }
+		thisAndTheNext = function(){
+			if (done()) {
+				stopIt();
+			} else {
+				currentAngle += anglePerClick;
+				rotateThisNow(elem, currentAngle, cx, cy);
+				setTimeout(function(){ thisAndTheNext(); }, msPerClick );
+			}
+		}
+		thisAndTheNext();
+	};
+	rotateThisNow = function(elem, angle, cx, cy) {
+		var transValue = "rotate("+angle+", "+cx+", "+cy+")"
+		elem.attr('transform', transValue);
+	};
+	$('#fig-1 .left-half').click(function(){
+		rotateLeft();
+	});
+	$('#fig-1 .right-half').click(function(){
+		rotateRight();
+	});
 });
